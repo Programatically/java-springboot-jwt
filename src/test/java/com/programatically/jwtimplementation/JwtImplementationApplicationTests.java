9cbaef892f6de67ffbef1491286d7bf21294568d
DaniyalAkbar <daniyalakbar1217@gmail.com>
2021-12-20 19:40:03 +0500

    First Commit

---
+++ src/test/java/com/programatically/jwtimplementation/JwtImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programatically.jwtimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
